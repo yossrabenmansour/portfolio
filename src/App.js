a8a8b73abc4b912bc044b8069ba3050524390f03
unknown <yossrabenmansour53@gmail.com>
2025-01-19 11:41:16 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbar from './Components/NavBar'
+import 'bootstrap/dist/css/bootstrap.min.css'
+function App() {
+  return (
+    <div className="App">
+     <Navbar/>
+    </div>
+  );
+}
+
+export default App;
